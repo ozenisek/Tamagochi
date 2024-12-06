c2b73618ef34c07439cb356096f30ee11dfee647
ozenisek <o.zenisek@seznam.cz>
2024-12-06 09:28:36 +0100

    first commit

---
+++ Tamagochi - Pet/Pet.cs
@@ -0,0 +1,86 @@
+﻿using Pet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Pet
+
+{
+    public abstract class Pett
+    {
+        private int hunger = 50;
+        public int Hunger
+        {
+            get
+            {
+                return hunger;
+            }
+            set
+            {
+                hunger = value;
+
+                if (!IsAlive())
+                {
+                    throw new DeadPetException();
+                }
+            }
+        }
+        private Random random = new Random();
+        public int Do()
+        {
+            var toss = random.Next(0, 20);
+            Console.WriteLine(toss);
+            if (toss < 10)
+            {
+                Chill();
+            }
+            else if (toss < 16)
+            {
+                Play();
+            }
+            else
+            {
+                Feed();
+            }
+            return toss;
+        }
+        public int Boredom
+        {
+            get; private set;
+        }
+        public void Feed()
+        {
+            Hunger -= 15; Boredom += 5;
+            Console.WriteLine("Eats..");
+        }
+
+        public void Play()
+        {
+            Hunger += 28; Boredom -= 25;
+            Console.WriteLine("Plays..");
+        }
+        public void Chill()
+        {
+            Hunger += 10; Boredom += 10;
+            Console.WriteLine("Chilling..");
+        }
+
+        public bool IsAlive()
+        {
+            if (Hunger > 100 || Hunger < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public virtual string WhoAmI()
+        {
+            return "Lizard";
+        }
+    }
+}
---
+++ Tamagochi - Pet/Program.cs
@@ -0,0 +1,41 @@
+﻿using Pet;
+using Pet;
+
+try
+{
+    var pet = new Lizard();
+    while (true)
+    {
+        Console.WriteLine("Lizard");
+        Console.Write("Hunger: ");
+        Console.WriteLine(pet.Hunger);
+        Console.Write("Boredom: ");
+        Console.WriteLine(pet.Boredom);
+
+        var toss = pet.Do();
+        Console.WriteLine($"Toss was {toss}");
+        if (toss > 0)
+        {
+            var action = Console.ReadLine();
+            while (action != "exit")
+            {
+            if (action == "P")
+            {
+                pet.Play();
+                    break;
+            }
+
+            if (action == "F")
+            {
+                pet.Feed();
+                    break;
+            }
+            }
+        }
+        Thread.Sleep(500);
+    }
+}
+catch (DeadPetException)
+{
+    Console.WriteLine("");
+}
